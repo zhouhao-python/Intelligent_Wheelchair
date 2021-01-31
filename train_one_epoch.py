55df6d196bdff1ed93b37e6d6f276ee834dd2013
zhouhao-python <18346041070@163.com>
2021-02-01 00:17:28 +0800

    make_dataset

--- train_one_epoch.py
+++ train_one_epoch.py
@@ -19,33 +19,33 @@ def train_one_epoch(opt):
     net.to(device)
     net.train()
     # loss_function = nn.CrossEntropyLoss()  # 定义损失函数为交叉熵损失函数
-    loss_function = nn.BCELoss()  # 定义损失函数为交叉熵损失函数
+    loss_function = nn.BCELoss(reduction='mean')  # 定义损失函数为交叉熵损失函数
     optimizer = optim.Adam(net.parameters(), lr=0.001)  # 定义优化器（训练参数，学习率）
 
     # for epoch in range(opt.num_epochs):  # 一个epoch即对整个训练集进行一次训练
+
     running_loss = 0.0
     correct = 0
     total = 0
     time_start = time.perf_counter()
-
     for step, data in enumerate(train_dataloader,
                                 start=0):  # 遍历训练集，step从0开始计算
         inputs, labels = data # 获取训练集的图像和标签
         inputs, labels = inputs.to(device), labels.to(device)
-        print(f'labels before.{labels}')
+        #print(f'labels before.{labels}')
         optimizer.zero_grad()  # 清除历史梯度
 
         # forward + backward + optimize
         # outputs = net(inputs.permute(0,1,3,2))  # 正向传播
         outputs = net(inputs)  # 正向传播
-        print(f'debug outputs.shape,label.shape:{outputs.size(),labels.size()}')
+        #print(f'debug outputs.shape,label.shape:{outputs.size(),labels.size()}')
         # outputs = torch.max(outputs, dim=1)[1]
         loss = loss_function(outputs, labels)  # 计算损失
         loss.backward()  # 反向传播
         optimizer.step()  # 优化器更新参数
         # predict_y = torch.max(outputs, dim=1)[1]
         # outputs = outputs.numpy()
-        print(f'outputs:{outputs}\n,labels:{labels}')
+        #print(f'outputs:{outputs}\n,labels:{labels}')
         zero = torch.zeros_like(outputs)
         one = torch.ones_like(outputs)
         outputs = torch.where(outputs > 0.5, one, outputs)
@@ -55,8 +55,8 @@ def train_one_epoch(opt):
         #     outputs = 1
         # else:
         #     outputs = 0
-        print('debug', outputs.size())
-        print(f'outputs:{outputs}\n,labels:{labels}')
+        #print('debug', outputs.size())
+        #print(f'outputs:{outputs}\n,labels:{labels}')
         total += labels.size(0)
         outputs = outputs.cpu().detach().numpy()
         labels = labels.cpu().detach().numpy()
