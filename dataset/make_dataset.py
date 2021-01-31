4ffb9d192880e98ab993d78766828110a3a29ab8
zhouhao-python <18346041070@163.com>
2021-01-31 18:06:17 +0800

    make_dataset

--- dataset/make_dataset.py
+++ dataset/make_dataset.py
@@ -10,27 +10,46 @@ import os
 
 def generate(path):
     image_lists = os.listdir(path)
+    print(image_lists)
+    a=0
+    context_list=[]
     with open(path +'\labels.txt', 'a+') as f:
         for img_name in image_lists:
+            print(img_name)
             if img_name.split('_')[0] == 'F':
                 label = 1
             elif img_name.split('_')[0] == 'N':
                 label = 0
-            context = '{path}/{image_name} {label}\n'.format(path=path, image_name=img_name, label=label)
-            # filename = os.path.split(file)[0]
+            context = '{path}/{image_name},{label},'.format(path=path, image_name=img_name, label=label)
+            context = '{path}/{image_name},'.format(path=path, image_name=img_name)
+            context_list.append(context)
+            a+=1
+            if a%6==0:
+                temp1=""
+                for temp in context_list:
+                    temp = str(temp)
+                    temp1 += temp
+                if label==1:
+                    print("debug_label",label)
+                #context_list.append("\n")
+                    context_str=temp1+'1'+'\n'
+                else:
+                    context_str=temp1+"0"+"\n"
+                # filename = os.path.split(file)[0]
             # filetype = os.path.split(file)[1]
             # print(filename, filetype)
             # if filetype == '.txt':
             #     continue
             # name = '/teeth' + '/' + file + ' ' + str(int(label)) + '\n'
-            print(context)
-            f.write(context)
+                print(context_str)
+                f.write(context_str)
+                context_list=[]
     print("finished!")
 
 
 if __name__ == '__main__':
     # 看近处的物体为0 看远处的物体为1
-    # img_path = r'D:\研究生期间项目资料\Gaze-Estimation\code\modify_module\dataset\train'
-    img_path = r'D:\研究生期间项目资料\Gaze-Estimation\code\modify_module\dataset\val'
-    # img_path = r'D:\研究生期间项目资料\Gaze-Estimation\code\modify_module\dataset\test'
+    #img_path = r'D:\pycharm\modify_module\dataset\train'
+    #img_path = r'D:\pycharm\modify_module\dataset\test'
+    img_path = r'D:\pycharm\modify_module\dataset\val'
     generate(img_path)
