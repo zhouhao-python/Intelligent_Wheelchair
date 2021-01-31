0f12b44b9200414024d0f5d360b5f80186516c1a
zhouhao-python <18346041070@163.com>
2021-01-31 17:30:45 +0800

    option

---
+++ option/__init__.py
@@ -0,0 +1,9 @@
+'''
+Author: your name
+Date: 2021-01-25 13:17:43
+LastEditTime: 2021-01-25 14:21:10
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \modify_module\option\__init__.py
+'''
+from option.basic_option import BasciOption
